98a0470ebaa0fe329afa585a06e4dc2732076a7f
Danat Egimbay <94670879+danatqwer@users.noreply.github.com>
2025-03-29 12:40:43 +0500

    init

--- practice-1/Задания/Массивы/2.js
+++
@@ -1,9 +0,0 @@
-// 2. Функция для фильтрации четных чисел из массива
-
-function filterEvenNumbers(arr) {
-  return arr.filter((e) => e % 2 === 0);
-}
-
-console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6])); // [2, 4, 6]
-console.log(filterEvenNumbers([7, 9, 11])); // []
-console.log(filterEvenNumbers([12, 14, 15, 17])); // [12, 14]
--- practice-1/Задания/Объекты/1.js
+++
@@ -1,8 +0,0 @@
-// 1. Подсчет количества свойств в объекте
-
-function countProperties(obj) {
-    return Object.keys(obj).length
-}
-
-const user = { name: "Alice", age: 25, city: "New York" };
-console.log(countProperties(user)); // 3
